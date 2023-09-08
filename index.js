87d683a6a2ae5d2e639e70b4abec345c4927372c
Grant <grantrwozniak@gmail.com>
2023-09-08 17:07:12 -0400

    Cats

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats = ['Milo','Otis','Garfield']
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function  destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name){
+    const  copyOfCats=[...cats]
+    copyOfCats.push(name);
+    return copyOfCats
+}
+function prependCat(name){
+    const  copyOfCats=[...cats]
+    copyOfCats.unshift(name);
+    return copyOfCats
+}
+function removeLastCat(name){
+    const copyCats=[...cats]
+    copyCats.pop(name)
+    return copyCats
+}
+function removeFirstCat(name){
+    const copyCats=[...cats]
+    copyCats.shift(name)
+    return copyCats
+}
